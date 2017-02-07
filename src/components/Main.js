17c3e91d4c8e4f17a178ff9b3d7a8771745bfbe6
xkcoding <237497819@qq.com>
2017-02-07 13:26:00 +0800

    modify photos width and height

--- src/components/Main.js
+++ src/components/Main.js
@@ -69,12 +69,12 @@ class ImgFigure extends React.Component {
 
     return (
       <figure className={ imgFigureClassName } style={ styleObj } onClick={this.handleClick}>
-    <img src={this.props.data.imageURL} alt={this.props.data.title} className={'img-style'}/>
+    <img src={this.props.data.imageURL} alt={this.props.data.title}/>
         <figcaption>
           <h2 className="img-title">{this.props.data.title}</h2>
           <div className="img-back" onClick={this.handleClick}>
             <p>
-              {this.props.data.title}
+              {this.props.data.desc}
             </p>
           </div>
         </figcaption>
